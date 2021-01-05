101b4f3ab2e29017406b964b245a4b8829a3f188
thantienduc <43912850+thantienduc@users.noreply.github.com>
2021-01-04 21:14:57 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,7 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import MainStackNavigator from './Components/StackNavigator'
+
+export default function App() {
+  return <MainStackNavigator />
+}
